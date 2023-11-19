14b973331ac0af962f36c73e15f4766e36d3f66f
Sergio David Rincon Murcia <sergiorincon50@gmail.com>
2023-11-19 16:40:31 -0500

    No funciona por el momneto, problema el Setup.py
    El Setup.py lo que hace es decirle al pakage que necesita para que funcione correctamente, pero esto solo apliaca a pakage que tengan codigo en python  y contengna nodos y demas cosas, en este caso como estamos haciendo unpakage unicamnet para que gazebo nos mueste el robot no es necesario.

--- src/burger1_description/setup.py
+++ src/burger1_description/setup.py
@@ -4,22 +4,24 @@ package_name = 'burger1_description'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (f'share/{package_name}/resource', ['resource/burger1.urdf.xacro']),
+        (f'share/{package_name}/resource', ['resource/base_link.xacro']),
+        (f'share/{package_name}/launch', ['launch/gazebo_launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='sergio',
     maintainer_email='sergiorincon50@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Robot description package for Burger1',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            # Puedes agregar scripts de consola aquí si los necesitas en el futuro
         ],
     },
 )
