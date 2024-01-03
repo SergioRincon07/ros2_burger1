466fe6e0eff4909ee5daf0bc6055b8f07699515c
Sergio David Rincon Murcia <sergiorincon50@gmail.com>
2024-01-02 19:28:17 -0500

    Se actualiza la tasa de Refresco de 10Hz a 400Hz en Gazebo

--- src/burger1_description/launch/launch_sim.launch.py
+++ src/burger1_description/launch/launch_sim.launch.py
@@ -18,6 +18,7 @@ def generate_launch_description():
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
     package_name='burger1_description' #<--- CHANGE ME
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -29,6 +30,7 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
