f86d147e53729ec7659033baac86616ca6e539fb
Sergio David Rincon Murcia <sergiorincon50@gmail.com>
2024-01-06 17:13:16 -0500

    Adicion Paquete para el control del robot en C++
    Se adiciona el archivo visibility_control.h, el cual se encarga de :
    
    La finalidad de este archivo visibility_control.h es gestionar la visibilidad de los símbolos (funciones, variables, etc.) dentro del código del paquete ROS2 llamado ros2_control_demo_example_2. La gestión de visibilidad es especialmente crucial cuando se construye el código como una biblioteca dinámica (también conocida como DLL en Windows o shared library en sistemas basados en Unix/Linux).

---
+++ src/burger1_robot/include/burger1_robot/visibility_control.h
@@ -0,0 +1,76 @@
+// Copyright 2021 ros2_control Development Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* This header must be included by all rclcpp headers which declare symbols
+ * which are defined in the rclcpp library. When not building the rclcpp
+ * library, i.e. when using the headers in other package's code, the contents
+ * of this header change the visibility of certain symbols which the rclcpp
+ * library cannot have, but the consuming code must have inorder to link.
+ */
+
+#ifndef ROS2_CONTROL__VISIBILITY_CONTROL_H_
+#define ROS2_CONTROL__VISIBILITY_CONTROL_H_
+
+// This logic was borrowed (then namespaced) from the examples on the gcc wiki:
+//     https://gcc.gnu.org/wiki/Visibility
+
+// Directiva de preprocesador para evitar la inclusión múltiple
+#ifndef ROS2_CONTROL_BUILDING_DLL
+// Definir el identificador si no está definido, para evitar la inclusión múltiple
+#define ROS2_CONTROL_BUILDING_DLL
+#endif
+
+// Verificar el sistema operativo para definir las etiquetas de exportación e importación
+#if defined _WIN32 || defined __CYGWIN__
+#ifdef __GNUC__
+// Exportar símbolos en sistemas Windows y GCC
+#define ROS2_CONTROL_EXPORT __attribute__((dllexport))
+#define ROS2_CONTROL_IMPORT __attribute__((dllimport))
+#else
+// Exportar símbolos en sistemas Windows con compiladores distintos a GCC
+#define ROS2_CONTROL_EXPORT __declspec(dllexport)
+#define ROS2_CONTROL_IMPORT __declspec(dllimport)
+#endif
+
+// Seleccionar la etiqueta de exportación o importación según si se está construyendo la DLL
+#ifdef ROS2_CONTROL_BUILDING_DLL
+#define ROS2_CONTROL_PUBLIC ROS2_CONTROL_EXPORT
+#else
+#define ROS2_CONTROL_PUBLIC ROS2_CONTROL_IMPORT
+#endif
+
+// Definir tipos para la visibilidad pública y local
+#define ROS2_CONTROL_PUBLIC_TYPE ROS2_CONTROL_PUBLIC
+#define ROS2_CONTROL_LOCAL
+
+#else // No es un sistema Windows
+
+// Etiquetas de exportación e importación para sistemas basados en Unix (Linux)
+#define ROS2_CONTROL_EXPORT __attribute__((visibility("default")))
+#define ROS2_CONTROL_IMPORT
+
+// Configurar la visibilidad pública y local según la versión de GCC
+#if __GNUC__ >= 4
+#define ROS2_CONTROL_PUBLIC __attribute__((visibility("default")))
+#define ROS2_CONTROL_LOCAL __attribute__((visibility("hidden")))
+#else
+#define ROS2_CONTROL_PUBLIC
+#define ROS2_CONTROL_LOCAL
+#endif
+
+// Definir tipos para la visibilidad pública y local
+#define ROS2_CONTROL_PUBLIC_TYPE
+#endif
+
+#endif  // ROS2_CONTROL__VISIBILITY_CONTROL_H_
